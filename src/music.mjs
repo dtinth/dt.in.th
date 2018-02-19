09edefa9282d4f50ea570175718e3a03890c163b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-02-19 22:36:17 +0700

    :memo: Add some of my discography for music page

---
+++ src/music.mjs
@@ -0,0 +1,96 @@
+
+export const songs = [
+  {
+    title: 'Butter-Fly (Digimon Tri Ver.) (Cover/Collab)',
+    artist: 'original by Wada Kouji · cover by MindaRyn',
+    type: 'cover',
+    genre: 'ANIME SONG COLLABORATION',
+    youtube: 'CHarkZrQH34',
+    date: '2017-01-20'
+  },
+  {
+    title: 'Everyday evermore',
+    artist: 'flicknote vs Dekdekbaloo feat. MindaRyn',
+    genre: 'HOPEFUL LOVE SONG',
+    youtube: 'KEqiqYXuaj8',
+    date: '2016-09-23'
+  },
+  {
+    title: 'Everyday evermore',
+    artist: 'flicknote · video by 5argon',
+    genre: 'MOR LAM SING STYLE',
+    youtube: 'KEqiqYXuaj8',
+    date: '2016-09-23'
+  },
+  {
+    title: 'OVER-WHELMING JOY',
+    artist: 'flicknote',
+    genre: 'SPEED CARNIVAL',
+    youtube: '0eRTa2NQ4r0',
+    date: '2016-07-03'
+  },
+  {
+    title: 'Running Out 2015',
+    artist: 'flicknote · video by 5argon',
+    genre: 'FRANTIC',
+    youtube: 'X6y2m09V4Hw',
+    date: '2016-07-03'
+  },
+  {
+    title: 'Only Love (Euphoric Trance Remix)',
+    artist: 'Shannon Hurley remixed by flicknote',
+    genre: 'EUPHORIC TRANCE',
+    youtube: 'KUoi1Hp-bzM',
+    date: '2015-08-16'
+  },
+  {
+    title: 'BY☆MY☆SIDE (Ambient House Mix)',
+    artist: 'flicknote',
+    genre: 'AMBIENT HOUSE',
+    soundcloud: '207325787',
+    date: '2015-05-27'
+  },
+  {
+    title: 'AUTO±SYNCHRO',
+    artist: 'flicknote',
+    genre: 'HARDSTYLE',
+    soundcloud: '186916227',
+    date: '2015-01-20'
+  },
+  {
+    title: 'Reminiscentia',
+    artist: 'flicknote',
+    genre: 'DRAMATIC TRANCE',
+    soundcloud: '171200575',
+    date: '2014-10-08'
+  },
+  {
+    title: 'BY☆MY☆SIDE',
+    artist: 'flicknote',
+    genre: 'TRANCE CORE',
+    soundcloud: '126044912',
+    date: '2013-12-23'
+  },
+  {
+    title: 'Sawasdee New Year',
+    artist: 'flicknote',
+    genre: 'CYBER QUICK WALTZ',
+    soundcloud: '73064541',
+    date: '2012-12-31'
+  },
+  {
+    title: 'Don’t Wake Me Up (iaht’s Re-Remix)',
+    artist: 'original by Addict Animal vs Surafux · remixed by iaht',
+    type: 'remix',
+    genre: 'MELODIC TRANCE',
+    soundcloud: '55936013',
+    date: '2012-08-11'
+  },
+  {
+    title: 'Opaque Space',
+    artist: 'iaht',
+    genre: 'HARDCORE',
+    soundcloud: '182770407',
+    date: '2010-09-10'
+  }
+]
