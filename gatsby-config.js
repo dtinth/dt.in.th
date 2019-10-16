671eb402bc26354f28ccb805b0f9dac52aa4b1d2
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-10-16 08:03:54 +0700

    Add Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     { resolve: `gatsby-plugin-mdx`, options: { commonmark: true } },
     {
       resolve: `gatsby-plugin-google-analytics`,
