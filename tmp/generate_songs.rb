c6a175c63e0b1f1ec4a3aa3d1e06d4d7f3fb9392
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-10-16 08:13:45 +0700

    Remove data extraction script

--- tmp/generate_songs.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-require 'pathname'
-
-JSON.parse(File.read('tmp/songs.json')).each do |id, contents|
-  target = Pathname.new("src/songs/#{id}.mdx")
-  target.parent.mkpath
-  target.write(contents)
-end
--- tmp/generate_talks.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-require 'pathname'
-
-JSON.parse(File.read('tmp/talks.json')).each do |id, contents|
-  target = Pathname.new("src/talks/#{id}.mdx")
-  target.parent.mkpath
-  target.write("---\n#{contents}---\n")
-end
