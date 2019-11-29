92acff54f052b7c2f96b905d7b6f249610e02fab
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-11-29 23:49:24 +0900

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
     { resolve: `gatsby-plugin-mdx`, options: { commonmark: true } },
     {
       resolve: `gatsby-plugin-google-analytics`,
