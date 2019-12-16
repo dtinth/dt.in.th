9babfbbaca39aa2d45b2891af2494764e6d32e17
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-12-16 11:07:50 +0800

    https://github.com/netlify/netlify-cms/issues/1408#issuecomment-526851039

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,17 +1,43 @@
 import CMS from 'netlify-cms-app'
-import React from 'react'
 import { StuffInfo } from '../stuff/StuffInfoPage'
 import PageWrapper from '../PageWrapper'
+import React, { useState, useEffect } from 'react'
+import { StyleSheetManager } from 'styled-components'
 
-CMS.registerPreviewTemplate('stuff', function StuffPreview({
-  entry,
-  widgetFor,
-}) {
+// https://github.com/netlify/netlify-cms/issues/1408#issuecomment-526851039
+function StyleInjector({ children }) {
+  const [iframeRef, setIframeRef] = useState(null)
+
+  useEffect(() => {
+    const iframe = document.getElementsByTagName('iframe')[0]
+    const iframeHeadElem = iframe.contentDocument.head
+    setIframeRef(iframeHeadElem)
+  }, [])
+
   return (
-    <PageWrapper>
-      <StuffInfo title={entry.getIn(['data', 'title'])}>
-        {widgetFor('body')}
-      </StuffInfo>
-    </PageWrapper>
+    iframeRef && (
+      <StyleSheetManager target={iframeRef}>{children}</StyleSheetManager>
+    )
   )
-})
+}
+
+export default function withStyledComponentsRendered(Comp) {
+  return props => (
+    <StyleInjector>
+      <Comp {...props} />
+    </StyleInjector>
+  )
+}
+
+CMS.registerPreviewTemplate(
+  'stuff',
+  withStyledComponentsRendered(function StuffPreview({ entry, widgetFor }) {
+    return (
+      <PageWrapper>
+        <StuffInfo title={entry.getIn(['data', 'title'])}>
+          {widgetFor('body')}
+        </StuffInfo>
+      </PageWrapper>
+    )
+  }),
+)
