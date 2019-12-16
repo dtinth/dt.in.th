9e0b01864ed1bd06e2a6f20f6cda978e7a066d2c
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-12-16 11:04:44 +0800

    Add PageWrapper

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,17 @@
 import CMS from 'netlify-cms-app'
 import React from 'react'
 import { StuffInfo } from '../stuff/StuffInfoPage'
+import PageWrapper from '../PageWrapper'
 
 CMS.registerPreviewTemplate('stuff', function StuffPreview({
   entry,
   widgetFor,
 }) {
   return (
-    <StuffInfo title={entry.getIn(['data', 'title'])}>
-      {widgetFor('body')}
-    </StuffInfo>
+    <PageWrapper>
+      <StuffInfo title={entry.getIn(['data', 'title'])}>
+        {widgetFor('body')}
+      </StuffInfo>
+    </PageWrapper>
   )
 })
