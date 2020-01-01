57784489d9cd803cdfbacf53765306eabc5f5f8c
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2020-01-02 03:05:02 +0700

    Add StuffListConnector

---
+++ src/stuff/StuffListConnector.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query StuffListConnectorQuery {
+        allMarkdownRemark(
+          filter: { fields: { sourceInstanceName: { eq: "stuff" } } }
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                tags
+                title
+                parent
+                description
+                created
+                updated
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data =>
+      props.children(data.allMarkdownRemark.edges.map(edge => edge.node))
+    }
+  />
+)
