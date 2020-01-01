29ce80f9459fecc26128df314fd988552bf9dc0b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2020-01-02 03:19:34 +0700

    Add id frontmatter to StuffListConnector

--- src/stuff/StuffListConnector.js
+++ src/stuff/StuffListConnector.js
@@ -15,6 +15,7 @@ export default props => (
                 slug
               }
               frontmatter {
+                id
                 tags
                 title
                 parent
