bb3554520bab02f7ab0cc99d8ab59225a489a256
dtinth on Cloud9 <dtinth@spacet.me>
2020-11-28 06:29:46 +0000

    Add some comments

--- .vuepress/lib/routeAnimations.js
+++ .vuepress/lib/routeAnimations.js
@@ -11,12 +11,22 @@ export const activeRouteAnimation = {
  */
 
 /**
+ * Request for a route animation to take place.
+ * Once called, the next route transition will invoke `fn`
+ * and will block until `fn` calls the `next()` function.
+ *
  * @param {RouteAnimation} fn
  */
 export function requestRouteAnimation(fn) {
   registeredRouteAnimation.current = fn
 }
 
+/**
+ * Signifies that a route animation is taking place.
+ * This can be used to avoid rendering slow components
+ * (such as a YouTube embed) while the animation is running
+ * to keep it running smoothly.
+ */
 export function activateRouteAnimation(timeout = 5000) {
   /** @type {() => void} */
   let resolver = () => {}
